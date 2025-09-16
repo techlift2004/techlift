88300e4b7f5acaf0df7ed23e54d601a7427e6c91
Cyberbot501 <softwareengineerjoycy@gmail.com>
2025-09-16 15:29:07 +0100

    done something

---
+++ src/layout/nav/nav.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import logo from '@/assets/techlift.svg'
+import { Button } from '@/components/ui/button'
+import { Link } from 'react-router-dom'
+
+export default function nav() {
+  return (
+    <div className='w-full flex flex-row justify-between items-center px-10 py-5 bg-white shadow-md'>
+
+        <div className=''>
+            <img src={logo} alt="logo" />
+        </div>
+
+        <ul className='flex flex-row gap-10 items-center font-Nunito text-'>
+            <Link to='/'>Home</Link>
+            <Link to='/about' >About</Link>
+            <Link>Blog</Link>
+            <Link>Events</Link>
+            <Link>Resources</Link>
+            <Link>Sponsor Us</Link>
+            <Link>Contact Us</Link>
+        </ul>
+
+        <Button className='bg-button-background '>Join The Community</Button>
+
+    </div>
+  )
+}
--- src/route.jsx
+++ src/route.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
 import Landing from "./pages/landing/page";
+import About from "./pages/about/page";
 
 
 export default function Router () {
@@ -8,6 +9,12 @@ export default function Router () {
     {
         path: "/",
         element: <Landing />,
+        index: true,
+    },
+
+     {
+        path: "/about",
+        element: <About />,
         index: true,
     },
   ]);
