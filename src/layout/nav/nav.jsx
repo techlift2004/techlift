6d9cd7c36fc35e7f9033ac5b247b6540710848c7
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2025-09-22 17:04:04 +0100

    done nav completely for all screens just waiting for the ui designer for adjustments(1)

--- src/layout/nav/nav.jsx
+++ src/layout/nav/nav.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import logo from "@/assets/techlift.svg";
-import bars from "../../assets/icons/hamburger.svg";
+// import bars from "../../assets/icons/hamburger.svg";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 
 export default function Nav() {
-  const [smallScreen, setSmallScreen] = useState(false);
+  // const [smallScreen, setSmallScreen] = Reat.useState(false);
 
   const links = [
     { to: "/", label: "Home" },
@@ -19,25 +19,25 @@ export default function Nav() {
 
   return (
     <div className="w-full flex flex-row justify-between items-center px-10 py-5 bg-white shadow-md">
-      <div className="flex justify-between items-center w-full">
-        <img src={logo} alt="logo" />
-        <img
+      {/* <div className="flex justify-between items-center w-full"> */}
+      <img src={logo} alt="logo" />
+      {/* <img
           src={bars}
           alt="navlinks-controller"
           onClick={() => setSmallScreen((prev) => !prev)}
           className="p-2 block lg:hidden hover:border-2 border-purple-600 hover:rounded-md"
-        />
-      </div>
-      <div className={`${smallScreen ? "block" : "hidden"}`}>
-        <ul className="flex flex-col lg:flex-row gap-10 items-center font-Nunito">
-          {links.map((l, i) => (
-            <Link key={i} to={l.to} className="hover:text-purple-600">
-              {l.label}
-            </Link>
-          ))}
-        </ul>
-        <Button className="bg-button-background">Join The Community</Button>
-      </div>
+        /> */}
+      {/* </div> */}
+      {/* <div className={`${smallScreen ? "block" : "hidden"}`}> */}
+      <ul className="flex flex-col lg:flex-row gap-10 items-center font-Nunito">
+        {links.map((l, i) => (
+          <Link key={i} to={l.to} className="hover:text-purple-600">
+            {l.label}
+          </Link>
+        ))}
+      </ul>
+      <Button className="bg-button-background">Join The Community</Button>
     </div>
+    // </div>
   );
 }
