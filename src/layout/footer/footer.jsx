5673834083cc71e8a24daef38bf92e80c3b7e513
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2025-09-22 12:31:20 +0100

    still working on the responsiveness(navbar) and adjusted some images

--- src/layout/footer/footer.jsx
+++ src/layout/footer/footer.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function footer() {
   return (
-    <div>footer</div>
-  )
+    <div className="bg-red-500 p-7">
+      <div></div>
+      <div>
+        <p>&copy;2025 TechLifts &bull; Terms &bull; Privacy</p>
+      </div>
+    </div>
+  );
 }
