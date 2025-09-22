8a30d4bbe294e1340d28ed22b8f1b30958243688
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2025-09-22 11:45:43 +0100

    updated node modules

--- src/layout/nav/nav.jsx
+++ src/layout/nav/nav.jsx
@@ -1,28 +1,26 @@
-import React from 'react'
-import logo from '@/assets/techlift.svg'
-import { Button } from '@/components/ui/button'
-import { Link } from 'react-router-dom'
+import React from "react";
+import logo from "@/assets/techlift.svg";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <div className='w-full flex flex-row justify-between items-center px-10 py-5 bg-white shadow-md'>
+    <div className="w-full flex flex-row justify-between items-center px-10 py-5 bg-white shadow-md">
+      <div className="">
+        <img src={logo} alt="logo" />
+      </div>
 
-        <div className=''>
-            <img src={logo} alt="logo" />
-        </div>
+      <ul className="flex flex-row gap-10 items-center font-Nunito text-">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link>Blog</Link>
+        <Link to="event">Events</Link>
+        <Link>Resources</Link>
+        <Link>Sponsor Us</Link>
+        <Link to="/contact">Contact Us</Link>
+      </ul>
 
-        <ul className='flex flex-row gap-10 items-center font-Nunito text-'>
-            <Link to='/'>Home</Link>
-            <Link to='/about' >About</Link>
-            <Link>Blog</Link>
-            <Link to='event'>Events</Link>
-            <Link>Resources</Link>
-            <Link>Sponsor Us</Link>
-            <Link to='/contact'>Contact Us</Link>
-        </ul>
-
-        <Button className='bg-button-background '>Join The Community</Button>
-
+      <Button className="bg-button-background ">Join The Community</Button>
     </div>
-  )
+  );
 }
