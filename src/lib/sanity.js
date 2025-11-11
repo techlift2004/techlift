c4c238e0d1ffede284fdfee162c0a179087c9ef9
Cyberbot501 <softwareengineerjoycy@gmail.com>
2025-11-11 19:47:55 +0100

    blog

---
+++ src/lib/sanity.js
@@ -0,0 +1,8 @@
+import { createClient } from "@sanity/client";
+
+export const client = createClient({
+  projectId: "8d1i38xm",
+  dataset: "production",
+  apiVersion: "2024-01-01",
+  useCdn: false,
+});
